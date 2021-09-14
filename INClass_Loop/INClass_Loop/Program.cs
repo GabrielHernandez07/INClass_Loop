b6be50852d9519b3dae82c54fab76f65fc26c80c
ghern <ghern@DESKTOP-OCUVVH0>
2021-09-14 11:39:01 -0500

    Loop Example

---
+++ INClass_Loop/INClass_Loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace INClass_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What do you wnat to count by? >>");
+            int countBy = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What do you wnat to count up to? >>");
+            int ceiling = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = countBy; i < ceiling; i = i + countBy)
+            {
+                if ((i)
+                {
+                    Console.Write(i.ToString("N0"));
+                } 
+
+            }
+
+            
+        }
+    }
+}
---
+++ INClass_Loop/INClass_Loop_Ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace INClass_Loop_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfExams = 0;
+            double totalOfExamScores = 0;
+            string answer;
+
+            do
+            {
+                Console.WriteLine("What is your exam score? >>");
+                double score = Convert.ToDouble(Console.ReadLine());
+                totalOfExamScores = totalOfExamScores + score;
+
+                numberOfExams = numberOfExams + 1;
+
+                Console.WriteLine("Do you have another exam score to enter? yes or no? >>");
+                answer = Console.ReadLine();
+            } while (answer == "yes");
+
+            double averageExamScore = totalOfExamScores / numberOfExams;
+            averageExamScore = averageExamScore / 100;
+            Console.WriteLine($"Your average exam score is {averageExamScore.ToString("P")}");
+
+        }
+    }
+}
